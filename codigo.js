3ddb75ce60641e368e119f6b76bed14e24eb4320
carlossrobayo <44556035+carlossrobayo@users.noreply.github.com>
2018-12-09 12:26:25 -0500

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,6 @@
+// Al cargarse el archivo JavaScript, se muestra un mensaje
+alert("Bienvenido!");
+ 
+// Despues del primer mensaje, se muestra otro mensaje seguido
+alert("Materia: PSP, Politecnico Grancolombiano - 2018 ");
+
